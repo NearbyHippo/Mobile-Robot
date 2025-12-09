9c85de84b8bc14131cd2acf05c03a2d89eb29493
NearbyHippo <sjfoley@buffalo.edu>
2025-12-08 22:18:43 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,20 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+// Define pins to control ultrasonic sensors
+
 const int frontTrigger = 2;
 const int frontEcho = 3;
 const int leftTrigger = 6;
 const int leftEcho = 7;
 const int rightTrigger = 4;
 const int rightEcho = 5;
+// These variables will store the output from the ultrasonic sensors
 long frontDistance;
 long leftDistance;
 long rightDistance;
+
+// Define pins to control motors
 const int rightEnable = 9;
 const int leftEnable = 11;
 const int leftWheel1 = 13;
@@ -21,6 +26,8 @@ const int rightWheel1 = 12;
 const int rightWheel2 = 8;
 
 void setup() {
+
+    // Set pin behaviors
 
   pinMode(frontTrigger, OUTPUT);
   pinMode(frontEcho, INPUT);
@@ -35,6 +42,8 @@ void setup() {
   pinMode(rightWheel1, OUTPUT);
   pinMode(rightWheel2, OUTPUT);
 
+    // Serial monitor for debugging
+
   Serial.begin(9600);
 
 }
@@ -44,15 +53,15 @@ void loop() {
   // From Arduino Project Hub: the distance of an object in centimeters is the pulse duration * 0.0343/2 https://projecthub.arduino.cc/Isaac100/getting-started-with-the-hc-sr04-ultrasonic-sensor-7cabe1
   digitalWrite(frontTrigger, LOW);
   delayMicroseconds(2);
-  digitalWrite(frontTrigger, HIGH);
+  digitalWrite(frontTrigger, HIGH);// tells front sensor to send a pulse
   delayMicroseconds(10); // https://projecthub.arduino.cc/Isaac100/getting-started-with-the-hc-sr04-ultrasonic-sensor-7cabe1
   digitalWrite(frontTrigger, LOW);
-  frontDistance = pulseIn(frontEcho, HIGH);
+  frontDistance = pulseIn(frontEcho, HIGH);// gets distance from pulse
 
   digitalWrite(rightTrigger, LOW);
   delayMicroseconds(2);
   digitalWrite(rightTrigger, HIGH);
-  delayMicroseconds(10);
+  delayMicroseconds(10);// allows for soundwaves to travel
   digitalWrite(rightTrigger, LOW);
   rightDistance = pulseIn(rightEcho, HIGH);
 
@@ -69,16 +78,16 @@ void loop() {
 
   if(frontDistance > 2000){
 
-    digitalWrite(rightEnable, HIGH);
+    digitalWrite(rightEnable, HIGH); // enable pins are left high allowing for motor control
     digitalWrite(leftEnable,HIGH);
 
 
-    digitalWrite(leftWheel2, HIGH);
+    digitalWrite(leftWheel2, HIGH); // if there are no objects in the car's immediate vicinity it will drive forward
     digitalWrite(rightWheel2, HIGH);
 
       if(rightDistance < 1000){
 
-      digitalWrite(leftWheel2, LOW);
+      digitalWrite(leftWheel2, LOW); // if there is an object to the car's right it turns left by stopping the left wheel briefly, then continues forward
       delay(1000);
 
       }
@@ -86,7 +95,7 @@ void loop() {
 
       else if(leftDistance < 1000){
 
-      digitalWrite(rightWheel2, LOW);
+      digitalWrite(rightWheel2, LOW);// if there is an object to the car's left it turns right by stopping the right wheel briefly, then continues forward
       delay(1000);
 
       }
@@ -95,17 +104,17 @@ void loop() {
 
   if(frontDistance < 2000){
 
-   digitalWrite(leftWheel2, LOW);
+   digitalWrite(leftWheel2, LOW); // if there is an object in front of the car it stops
    digitalWrite(rightWheel2, LOW);
 
-   if(leftDistance < rightDistance){
+   if(leftDistance < rightDistance){ // the car decides which way to go
 
-    digitalWrite(leftWheel2, HIGH);
+    digitalWrite(leftWheel2, HIGH);// if there is an object closer to the left it will go right
     delay(1000);
     digitalWrite(leftWheel2, LOW);
 
    }
-   else if(leftDistance> rightDistance){
+   else if(leftDistance> rightDistance){// if there is an object closer to the right it will go left
 
     digitalWrite(rightWheel2, HIGH);
     delay(1000);
